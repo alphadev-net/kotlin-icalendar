35cc3f6d2fd42141379540f269bed186fb5fa01f
Jan Seeger <jan@alphadev.net>
2026-02-03 21:24:53 +0100

    Add Clikt cli options

--- app/src/net/alphadev/icalendar/cli/main.kt
+++ app/src/net/alphadev/icalendar/cli/main.kt
@@ -1,5 +1,54 @@
 package net.alphadev.icalendar.cli
 
-fun main(vararg args: String) {
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.arguments.convert
+import com.github.ajalt.clikt.parameters.groups.mutuallyExclusiveOptions
+import com.github.ajalt.clikt.parameters.groups.required
+import com.github.ajalt.clikt.parameters.groups.single
+import com.github.ajalt.clikt.parameters.options.convert
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.file
+import kotlinx.coroutines.runBlocking
+import kotlinx.io.files.Path
+import net.alphadev.icalendar.export.toICalString
+import kotlin.time.measureTime
 
+fun main(args: Array<String>) = iCalendar().main(args)
+
+private sealed class Source {
+    data class Url(val url: String): Source()
+    data class File(val path: Path): Source()
+}
+
+internal class iCalendar: CliktCommand(name = "icalendar") {
+    override val printHelpOnEmptyArgs = true
+
+    private val source: Source by mutuallyExclusiveOptions<Source>(
+        option("--file", "-f", metavar = "FILE").convert { Source.File(Path(it.toString())) },
+        option("--url", "-i").convert { Source.Url(it) },
+        name = "Source"
+    ).single().required()
+
+    private val output: Path? by option(
+        "--output", "-o", metavar = "FILE",
+        help = "If not specified, the output will be printed to STDOUT")
+        .file(mustExist = false, canBeDir = false, mustBeWritable = true)
+        .convert { Path(it.toString()) }
+
+    override fun run() {
+        val duration = measureTime {
+            val iCalContents = when (val source = source) {
+                is Source.File -> readICalFromFile(source.path)
+                is Source.Url -> runBlocking { readICalFromUrl(source.url) }
+            }
+
+            when (val output = output) {
+                null -> println(iCalContents.toICalString())
+                else -> iCalContents.writeICalFile(output)
+            }
+        }
+
+        println(" in $duration")
+    }
 }
