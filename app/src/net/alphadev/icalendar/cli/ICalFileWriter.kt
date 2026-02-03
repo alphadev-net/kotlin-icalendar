1643de5a7562916b80701c53401fbd3500355ec1
Jan Seeger <jan@alphadev.net>
2026-02-03 19:51:40 +0100

    Add File IO functions

---
+++ app/src/net/alphadev/icalendar/cli/ICalFileWriter.kt
@@ -0,0 +1,21 @@
+package net.alphadev.icalendar.cli
+
+import kotlinx.io.buffered
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
+import kotlinx.io.writeString
+import net.alphadev.icalendar.export.toICalString
+import net.alphadev.icalendar.model.VCalendar
+
+fun List<VCalendar>.writeICalFile(destFile: Path) {
+    try {
+        val fileContents = toICalString()
+
+        SystemFileSystem.sink(destFile)
+            .buffered()
+            .use { it.writeString(fileContents, 0, fileContents.length) }
+
+    } catch (ex: Throwable) {
+        ex.printStackTrace()
+    }
+}
