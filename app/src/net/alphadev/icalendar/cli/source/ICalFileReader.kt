03e05e486afdeedd455eee95cac7bd8ff5ecf368
Jan Seeger <jan@alphadev.net>
2026-03-10 23:58:42 +0100

    Switch ICalFileReader to use the new streaming API

--- app/src/net/alphadev/icalendar/cli/source/ICalFileReader.kt
+++ app/src/net/alphadev/icalendar/cli/source/ICalFileReader.kt
@@ -9,11 +9,10 @@ import net.alphadev.icalendar.model.VCalendar
 
 fun readICalFromFile(file: Path): List<VCalendar> {
     return try {
-        val fileContents = SystemFileSystem.source(file)
+        val fileSource = SystemFileSystem.source(file)
             .buffered()
-            .readString()
 
-        parseICalendar(fileContents)
+        parseICalendar(fileSource)
     } catch (ex: Throwable) {
         ex.printStackTrace()
         emptyList()
